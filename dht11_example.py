926e02f59005b90fef18d3baf156c9262122babb
JurassicPork <jurassic.pork@orange.fr>
2017-07-28 01:15:29 +0200

    new syntax for DHT constructor

--- dht11_example.py
+++ dht11_example.py
@@ -5,7 +5,7 @@ from dth import DTH
 
 pycom.heartbeat(False)
 pycom.rgbled(0x000008) # blue
-th = DTH(Pin('P3', mode=Pin.OPEN_DRAIN),0)
+th = DTH('P3',0)
 time.sleep(2)
 result = th.read()
 if result.is_valid():
